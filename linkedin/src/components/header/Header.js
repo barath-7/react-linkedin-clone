ec585b52e0b7704b8362f3a00b528731b4ac74cb
barath-7 <79738124+barath-7@users.noreply.github.com>
2022-04-05 23:57:19 +0530

    Added new icons in header

--- linkedin/src/components/header/Header.js
+++ linkedin/src/components/header/Header.js
@@ -6,6 +6,9 @@ import HeaderOption from './HeaderOption';
 import HomeIcon from '@mui/icons-material/Home';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import WorkIcon from '@mui/icons-material/Work';
+import ChatRoundedIcon from '@mui/icons-material/ChatRounded';
+import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded';
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 
 const Header = () => {
   return (
@@ -28,6 +31,9 @@ const Header = () => {
     <HeaderOption title='Home' Icon={HomeIcon}/>
     <HeaderOption title='My Network' Icon={PeopleAltIcon}/>
     <HeaderOption title='Jobs' Icon={WorkIcon}/>
+    <HeaderOption title='Messaging' Icon={ChatRoundedIcon}/>
+    <HeaderOption title='Notification' Icon={NotificationsRoundedIcon}/>
+    <HeaderOption title='Me' Icon={AccountCircleRoundedIcon}/>
       </div>
     </div>
   );
