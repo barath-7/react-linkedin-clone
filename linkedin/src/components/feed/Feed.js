a87a755c2ba7341d2b220da2b4da4ad78e016bd0
barath-7 <79738124+barath-7@users.noreply.github.com>
2022-04-09 18:52:52 +0530

    Added styling for feed input options

--- linkedin/src/components/feed/Feed.js
+++ linkedin/src/components/feed/Feed.js
@@ -1,7 +1,11 @@
 import React from "react";
 import './Feed.css'
 import CreateIcon from "@mui/icons-material/Create";
-
+import InputOption from "./InputOption";
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventIcon from '@mui/icons-material/Event';
+import NotesIcon from '@mui/icons-material/Notes';
 const Feed = () => {
   return (
     <div className="feed">
@@ -12,6 +16,12 @@ const Feed = () => {
               <input type="text" />
               <button type="submit">Send</button>
           </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption title='Photo' Icon={ImageIcon} color='#70B5F9'/>
+          <InputOption title='Video' Icon={SubscriptionsIcon} color='#E7A33E'/>
+          <InputOption title='Event' Icon={EventIcon} color='#C0CBCD'/>
+          <InputOption title='Write article' Icon={NotesIcon} color='#7FC15E'/>
         </div>
       </div>
     </div>
